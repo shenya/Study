8c2d437f7774e677008ee4202d5452dc89400ed7
shengy <shengybest@gmail.com>
2017-01-21 12:21:04 +0800

    Create initial main.c and Makefile

---
+++ Back/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc, char *argv[])
+{
+
+
+
+    printf("hello world\n");
+
+    return 0;
+}
